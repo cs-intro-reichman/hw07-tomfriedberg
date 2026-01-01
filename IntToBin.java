dfe74a7a8d9fab66bf0ced2df61997c4817a4375
Tom Friedberg <tomfriedberg@MacBook-Air-sl-Tom.local>
2026-01-01 23:33:43 +0200

    Finished hw07

--- IntToBin.java
+++ IntToBin.java
@@ -8,7 +8,9 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+        if (x <= 1) {
+            return Integer.toString(x);
+        }
+        return toBinary(x / 2) + (x % 2);
     }    
  }
--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,37 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		double[] x = {0, 1, 0.5};
+        double[] y = {0, 0, Math.sqrt(3) / 2};
+        
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.filledPolygon(x, y);
+        
+        sierpinski(n, x[0], x[1], x[2], y[0], y[1], y[2]);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n == 0) {
+        return;
+    }
+
+    double x12 = (x1 + x2) / 2;
+    double y12 = (y1 + y2) / 2;
+    double x23 = (x2 + x3) / 2;
+    double y23 = (y2 + y3) / 2;
+    double x31 = (x3 + x1) / 2;
+    double y31 = (y3 + y1) / 2;
+
+    double[] px = { x12, x23, x31 };
+    double[] py = { y12, y23, y31 };
+
+    StdDraw.setPenColor(StdDraw.WHITE);
+    StdDraw.filledPolygon(px, py);
+
+    sierpinski(n - 1, x1, x12, x31, y1, y12, y31);
+    sierpinski(n - 1, x12, x2, x23, y12, y2, y23);
+    sierpinski(n - 1, x31, x23, x3, y31, y23, y3);
 	}
 }
